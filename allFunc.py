c8051589f6cb70fb53dc72e0036cfd3e6cdff029
Maryam Sherif <mariamsherif2.ms@gmail.com>
2024-03-03 20:37:16 +0200

    final

--- allFunc.py
+++ allFunc.py
@@ -8,9 +8,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
-              
+
+def co_occurrence_matrix(img):
+
+    # Convert the image to a NumPy array
+    pixel_array = np.array(img)
+
+    # Reshape the 1D array to a 2D array
+    height, width = img.size
+    pixArr = pixel_array
+
+    # Verify the shape of the 2D array
+    print("Reshaped Shape:", pixArr.shape)
+
+    COoccurrenceMatrix = np.zeros((256, 256), dtype=np.uint32)
+
+
+    for x in range(pixArr.shape[0]):
+        for y in range(pixArr.shape[1]):
+            if x + 1 < pixArr.shape[0]:
+                val1 = pixArr[x][y]
+                val2 = pixArr[x + 1][y]
+                COoccurrenceMatrix[val1][val2] += 1
+
+    return COoccurrenceMatrix
+
+def contrast(matrix):
+  i=0
+  j=0
+  first=0
+  second=0
+  for i in range(255):
+    for j in range(255):
+      first+=matrix[i][j]* abs(i-j)
+  for i in range(255):
+    for j in range(255):
+     second+= abs(i-j)
+
+  contrast = first/second
+  return contrast
+
+#------------------------------------------------------------------------------------------------
+
 def calculate_histogram(image_array):
-   
+
     histogram = [0] * 256
 
     for i in range(len(image_array)):
@@ -32,9 +73,9 @@ def calculate_cumulative_histogram(histogram):
 
 
 def get_color_at_percentage(cumulative_histogram, percentage):
-    total_pixels = cumulative_histogram[-1]  
-    target_value = percentage * total_pixels / 100 
-    
+    total_pixels = cumulative_histogram[-1]
+    target_value = percentage * total_pixels / 100
+
     for i in range(len(cumulative_histogram)):
         if cumulative_histogram[i] >= target_value:
             lower_index = i
@@ -45,7 +86,7 @@ def get_color_at_percentage(cumulative_histogram, percentage):
             upper_index = i
             break
 
-    
+
     lower_intensity = lower_index
     upper_intensity = upper_index
 
@@ -61,15 +102,15 @@ def StretchContrast(img, a, b, c, d):
             if c <= pixel_value <= d:
                 img[i][j] = int(((pixel_value - c) * scaling_factor) + a)
 
-            # Clip the values to the range [a, b] which is [0,255] if there are outlying pixel values    
+            # Clip the values to the range [a, b] which is [0,255] if there are outlying pixel values
             elif pixel_value < c:
                 img[i][j] = a  # Clip to 'a' for values below 'c'
             else:
                 img[i][j] = b  # Clip to 'b' for values above 'd'
 
     return img
-        
-    
+
+
 def EqualizeHistogram(img, lower_intensity, higher_intensity):
     # Step 1: Calculate Histogram
     histogram = calculate_histogram(img)
@@ -106,7 +147,7 @@ def GrayScaleTransformation(img, x1, x2, y1, y2):
     if x1 >= x2 or y1 >= y2:
         print("Error: x1 must be less than x2 and y1 must be less than y2")
         return img
-    
+
     for i in range(len(img)):
         for j in range (len(img[i])):
             pixel_value = img[i][j]
@@ -118,16 +159,26 @@ def GrayScaleTransformation(img, x1, x2, y1, y2):
                 img[i][j] = int(((pixel_value - x2) * ((255-y2)/(255-x2))) + y2)
 
     return img
-
+#------------------------------------------------------------------------------------------------
 #main function
 if __name__ == "__main__":
 
     # Enter the image path
-    image_path = "test/3.png"
+    image_path = "test/1.png"
 
     im = Image.open(image_path)
     gray_scale = im.convert("L")
     image_array = [[gray_scale.getpixel((i, j)) for j in range(im.size[1])] for i in range(im.size[0])]
+
+    #Calculate the co-occurrence matrix
+    co_occurrence_matrix = co_occurrence_matrix(im)
+    print("Co-Occurrence matrix of the test image: ")
+    print(co_occurrence_matrix)
+
+    #Calculate the contrast of the co-occurrence matrix
+    contrast = contrast(co_occurrence_matrix)
+    print("Contrast of the co-occurrence matrix: ")
+    print(contrast)
 
     #Calculate the histogram
     histogram = calculate_histogram(image_array)
@@ -143,13 +194,13 @@ if __name__ == "__main__":
     print("Lower Intensity:", lower_intensity)
     print("Upper Intensity:", upper_intensity)
 
-    # # Stretch Contrast: 
+    # # Stretch Contrast:
     # stretched_image = StretchContrast(image_array, 0, 255, 88, 150)
     # stretched_image = np.transpose(stretched_image)
     # plt.subplot(121), plt.imshow(gray_scale, cmap='gray'), plt.title('Original Image')
     # plt.subplot(122), plt.imshow(stretched_image, cmap='gray'), plt.title('Stretched Image')
     # plt.show()
-    
+
     # #Equalize Histogram:
     # equalized_image = EqualizeHistogram(image_array, lower_intensity, upper_intensity)
     # equalized_image = np.transpose(equalized_image)
@@ -157,11 +208,11 @@ if __name__ == "__main__":
     # plt.subplot(122), plt.imshow(equalized_image, cmap='gray'), plt.title('Equalized Image')
     # plt.show()
 
-    # #Gray Scale Transformation:
-    # transformed_image = GrayScaleTransformation(image_array, lower_intensity, upper_intensity, 10, 200)
-    # transformed_image = np.transpose(transformed_image)
-    # plt.subplot(121), plt.imshow(gray_scale, cmap='gray'), plt.title('Original Image')
-    # plt.subplot(122), plt.imshow(transformed_image, cmap='gray'), plt.title('Transformed Image')
-    # plt.show()
+    #Gray Scale Transformation:
+    transformed_image = GrayScaleTransformation(image_array, lower_intensity, upper_intensity, 10, 200)
+    transformed_image = np.transpose(transformed_image)
+    plt.subplot(121), plt.imshow(gray_scale, cmap='gray'), plt.title('Original Image')
+    plt.subplot(122), plt.imshow(transformed_image, cmap='gray'), plt.title('Transformed Image')
+    plt.show()
 
     pass
